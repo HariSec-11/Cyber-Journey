11dc9bfa118bf7d3f89d138d68a3087cdcf4b826
Hari <hari@example.com>
2025-10-09 18:08:13 +0530

    Init: README for SentinelLite / Cyber Journey

--- README.md
+++
@@ -1,1 +0,0 @@
-﻿## SentinelLite - Cyber Journey
--- system_info.py
+++
@@ -1,7 +0,0 @@
-import os
-import platform
-
-print("Current User:", os.getlogin())
-print("Architecture:", platform.architecture())
-print("Python Version:", platform.python_version)
-
